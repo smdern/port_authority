be2adcb48c9115ccb466244392a9798b95198732
Shaun Dern <shaun@substantial.com>
2014-05-16 15:27:10 +0200

    Cleanup session

--- lib/port_authority/session.rb
+++ lib/port_authority/session.rb
@@ -1,17 +1,18 @@
 require 'net/sftp'
 
-class PortAuthority::Session
+module PortAuthority
+  class Session
 
-  attr_reader :host, :username, :options
+    attr_reader :host, :username, :options
 
-  def initialize(host, username, options={})
-    @host = host
-    @username = username
-    @options = options
-  end
+    def initialize(host, username, options={})
+      @host = host
+      @username = username
+      @options = options
+    end
 
-  def start(&block)
-    Net::SFTP.start(@host, @username, @options, &block) if block_given?
+    def start(&block)
+      Net::SFTP.start(host, username, options, &block) if block_given?
+    end
   end
 end
-
